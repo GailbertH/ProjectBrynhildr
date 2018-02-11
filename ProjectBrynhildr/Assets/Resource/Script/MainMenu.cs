f71f4110412c0dd56d126e310825900c82bef224
Gailbert Huang <gailbert.huang@gmail.com>
2018-02-11 20:42:57 +0800

    Loading Screen and Scene Transition Manager

---
+++ ProjectBrynhildr/Assets/Resource/Script/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour 
+{
+	public void StartGameClicked()
+	{
+		LoadingManager.Instance.SetSceneToUnload (SceneNames.MAIN_MENU);
+		LoadingManager.Instance.SetSceneToLoad (SceneNames.GAME_UI + "," + SceneNames.GAME_SCENE);
+		LoadingManager.Instance.LoadGameScene ();
+	}
+}
