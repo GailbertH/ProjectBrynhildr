0f6ed8a8aa2bffe1d38bc23e73f7ba1e2eef70c1
Gailbert Huang <gailbert.h@anino.co>
2018-03-21 22:55:08 +0800

    controller.

--- ProjectBrynhildr/Assets/Resource/Script/Player/PlayerController.cs
+++ ProjectBrynhildr/Assets/Resource/Script/Player/PlayerController.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour 
+namespace Brynhildr.Player
 {
-
+	public class PlayersHandler : MonoBehaviour 
+	{
+		[SerializeField] private CharacterController charController;
+		[SerializeField] private Animator playerAnimtor;
+		[SerializeField] private GameObject playerChar;
+		[SerializeField] private GameObject cameraHolder;
+		[SerializeField] public PlayerData playerData;
+	}
 }
