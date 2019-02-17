2e9f918cd6c8b9efe0a1babd8c16fcffb584c4c6
Gailbert Huang <gailbert.h@anino.co>
2019-02-17 19:16:24 +0800

    something

--- ProjectBrynhildr/Assets/Resource/Script/Enemy/BossController.cs
+++ ProjectBrynhildr/Assets/Resource/Script/Enemy/BossController.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Brynhildr.Enemy;
+using Brynhildr.Player;
 
 public class BossController : EnemyController
 {
-	
+	private GateController targetGate = null;
+	private PlayerController targetPlayer = null;
 }
 
